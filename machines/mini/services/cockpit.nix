3ab946c4fc684e2b1fcc391b6610449fbccb3fe6
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-04-07 03:05:23 +0200

    machines/mini/services/cockpit.nix: enable proxies

--- machines/mini/services/cockpit.nix
+++ machines/mini/services/cockpit.nix
@@ -10,4 +10,11 @@
   ];
 
   services.cockpit.enable = true;
+  services.cockpit.allowed-origins = [ "https://*:9090" "https://*:373" ];
+  services.cockpit.settings = {
+    WebService = {
+      AllowUnencrypted = true;
+      ProtocolHeader = "X-Forwarded-Proto";
+    };
+  };
 }
