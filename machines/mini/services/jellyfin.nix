6bb738eed25af6c4a39c3ac9e6ee8ff7e5817ab1
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-04-07 03:04:16 +0200

    machines/mini/services: add jellyfin

---
+++ machines/mini/services/jellyfin.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  services.jellyfin.enable = true;
+
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+}
