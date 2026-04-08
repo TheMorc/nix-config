24226cb785b5d609967a4e4bbaf932ced17254ed
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-04-08 20:43:59 +0200

    packages&machines: add tvh

---
+++ machines/mini/services/tvh.nix
@@ -0,0 +1,10 @@
+# kudos to ungeskriptet
+{ config, pkgs, inputs, ... }:
+{
+  imports = [ ../../../packages/tvh.nix ];
+  services.tvheadend2 = {
+    enable = true;
+    firstRun = true;
+    package = inputs.tvh.legacyPackages.${pkgs.stdenv.hostPlatform.system}.tvheadend;
+  };
+}
