10e94d3be80f69162151c12d2273fd1f18e26d39
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-04-10 00:18:56 +0200

    services: vnc.nix&vnc_ro.nix: start after we have network

--- machines/mini/services/vnc.nix
+++ machines/mini/services/vnc.nix
@@ -12,6 +12,8 @@
   systemd.user.services.vnc = {
     enable = true;
     wantedBy = [ "default.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.x11vnc}/bin/x11vnc -display :0 -rfbport 5901 -forever -shared";
--- machines/mini/services/vnc_ro.nix
+++ machines/mini/services/vnc_ro.nix
@@ -12,6 +12,8 @@
   systemd.user.services.vnc_ro = {
     enable = true;
     wantedBy = [ "default.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.x11vnc}/bin/x11vnc -display :0 -viewonly -forever -shared";
