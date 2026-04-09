10c4a68017f4948717033a9c4af51e6791a1a80d
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-04-10 00:04:23 +0200

    packages: add sqlite runtime dependency to mcgalaxy

--- packages/mcgalaxy.nix
+++ packages/mcgalaxy.nix
@@ -2,6 +2,7 @@
   buildDotnetModule,
   dotnetCorePackages,
   fetchFromGitHub,
+  sqlite,
 }:
 
 buildDotnetModule (finalAttrs: {
@@ -14,11 +15,13 @@ buildDotnetModule (finalAttrs: {
     tag = finalAttrs.version;
     hash = "sha256-dInKW6y8S0mzac2cAUg0RMrfaEThgNoyNTZU66TGIyA=";
   };
-  
+
   projectFile = "CLI/MCGalaxyCLI_dotnet8.csproj";
 
-  nugetDeps = ./mcgalaxy_deps.json; #nix-build -E "with import <nixpkgs> {}; (callPackage ./mcgalaxy.nix {}).fetch-deps"
-  
+  nugetDeps = ./mcgalaxy_deps.json; # nix-build -E "with import <nixpkgs> {}; (callPackage ./mcgalaxy.nix {}).fetch-deps" && ./result
+
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
+
+  runtimeDeps = [ sqlite ];
 })
