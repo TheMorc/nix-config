6fde9122b3228b828f4ece48905b5d74566a3a6b
Richard Gráčik <morc@370.network>
2026-04-17 20:27:31 +0200

    bigscreen/services: waydroid format

--- machines/bigscreen/services/waydroid.nix
+++ machines/bigscreen/services/waydroid.nix
@@ -9,20 +9,20 @@
   virtualisation.waydroid.enable = true;
 
   systemd.user.services.waydroid-session = {
-	enable = true;      
-	description = "Waydroid User Session";
-      after = [ "waydroid-container.service" ];
-      wantedBy = [ "default.target" ];
+    enable = true;
+    description = "Waydroid User Session";
+    after = [ "waydroid-container.service" ];
+    wantedBy = [ "default.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.waydroid}/bin/waydroid";
       ExecStop = "${pkgs.waydroid}/bin/waydroid container stop";
-      
+
       KillMode = "mixed";
-      
+
       MemoryMax = "4G";
       MemoryHigh = "3G";
-      
+
       TimeoutStopSec = "15s";
       Restart = "on-failure";
     };
