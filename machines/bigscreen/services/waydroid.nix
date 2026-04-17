491e9124e610d8ae0e3e0ac5452a860d1d939481
Richard Gráčik <morc@370.network>
2026-04-17 14:59:22 +0200

    machines/bigscreen/services/wayland.nix: reorganize

--- machines/bigscreen/services/waydroid.nix
+++ machines/bigscreen/services/waydroid.nix
@@ -8,16 +8,23 @@
 
   virtualisation.waydroid.enable = true;
 
-  systemd.user.services.waydroid = {
-    enable = true;
-    partOf = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    wantedBy = [ "graphical-session.target" ];
+  systemd.user.services.waydroid-session = {
+	enable = true;      
+	description = "Waydroid User Session";
+      after = [ "waydroid-container.service" ];
+      wantedBy = [ "default.target" ];
     serviceConfig = {
       Type = "simple";
       ExecStart = "${pkgs.waydroid}/bin/waydroid session start";
-      User = "htpc";
-      Group = "users";
+      ExecStop = "${pkgs.waydroid}/bin/waydroid session stop";
+      
+      KillMode = "mixed";
+      
+      MemoryMax = "4G";
+      MemoryHigh = "3G";
+      
+      TimeoutStopSec = "15s";
+      Restart = "on-failure";
     };
   };
 }
