5a3f79698fe8b6b18ad88ccdd7016866f3ab81cd
Leandro Friedrich <leandro@works-on-my.computer>
2026-07-18 16:28:47 +0200

    modules: enable sylinks on modules-default.nix

--- modules/modules-default.nix
+++ modules/modules-default.nix
@@ -3,7 +3,7 @@
   imports = lib.map (file: ./. + "/${file}") (
     lib.attrNames (
       lib.filterAttrs (
-        file: type: file != "default.nix" && type == "regular" && lib.hasSuffix ".nix" file
+        file: type: file != "default.nix" && lib.hasSuffix ".nix" file
       ) (builtins.readDir ./.)
     )
   );
